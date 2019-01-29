61e89a6252340155b819ce0434003b76c6cf9b3c
fahim-86 <famambission491@gmail.com>
2019-01-29 21:44:49 +0600

    Done creating Add Exprience form and it functionality

--- client/src/store.js
+++ client/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
-const middleware = [thunk];
+//const middleware = [thunk];
 
 /*const store = createStore(
   rootReducer,
