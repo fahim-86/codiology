47786a1d5654fed015e74c40e88e44de74a31553
fahim-86 <famambission491@gmail.com>
2019-02-01 14:56:38 +0600

    About section added on the profile list page

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -33,7 +33,7 @@ class Profile extends Component {
             <div className="col-md-6" />
           </div>
           <ProfileHeader profile={profile} />
-          <ProfileAbout />
+          <ProfileAbout profile={profile} />
           <ProfileCreds />
           <ProfileGithub />
         </div>
--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import isEmpty from '../../validation/is-empty';
 
 class ProfileAbout extends Component {
   render() {
-    return <h5>About section will be shown here</h5>;
+    const { profile } = this.props;
+
+    // Get First name only
+    const firstName = profile.user.name.trim().split(' ')[0];
+
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-light mb-3">
+            <h3 className="text-center text-info">{firstName}'s Biography</h3>
+            <p className="lead">
+              {isEmpty(profile.bio) ? (
+                <span>{firstName} hasn't given any information yet</span>
+              ) : (
+                <span>{profile.bio}</span>
+              )}
+            </p>
+            <hr />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
