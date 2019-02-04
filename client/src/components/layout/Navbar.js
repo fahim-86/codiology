29353dec13acc39412f756b5c915c03c2ff4af42
fahim-86 <famambission491@gmail.com>
2019-02-04 22:25:35 +0600

    Added a comment post section

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -18,6 +18,11 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <Link to="/feed" className="nav-link">
+            Post Feed
+          </Link>
+        </li>
+        <li className="nav-item">
           <Link className="nav-link" to="/dashboard">
             Dashboard
           </Link>
