141bc0f3fa2da0b244bc2c265ce71315eac356da
fahim-86 <famambission491@gmail.com>
2019-02-06 22:04:57 +0600

    Made the like button functional

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -63,6 +63,32 @@ export const deletePost = id => dispatch => {
     );
 };
 
+// Add Likes
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Remove Likes
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Set loading state
 export const setPostLoading = () => {
   return {
