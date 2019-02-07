7af68189aa0a8d574cef14b9ddf31b97980cc74d
fahim-86 <famambission491@gmail.com>
2019-02-07 21:33:43 +0600

    taking initiative to push my project to Heroku; I just hide my database key file

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://socmediaapp:famdude@ds019470.mlab.com:19470/socnetapp',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
