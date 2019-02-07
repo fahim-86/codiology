d1a6b4e518864af8e6060c9ba2f77696ff148549
fahim-86 <famambission491@gmail.com>
2019-02-07 12:18:02 +0600

    A bug fixed for user post comment section

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -112,7 +112,7 @@ export const removeLike = id => dispatch => {
 // Add Comment
 export const addComment = (postId, commentData) => dispatch => {
   axios
-    .post(`/api/posts/comment/${postId}`)
+    .post(`/api/posts/comment/${postId}`, commentData)
     .then(res =>
       dispatch({
         type: GET_POST,
