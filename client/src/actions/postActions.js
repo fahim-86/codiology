eb645ae8e3328c84b919d1ce49548c8fcca223eb
fahim-86 <famambission491@gmail.com>
2019-02-07 14:48:30 +0600

    Post Feed page updated, Now user will be able to make and delete comments for any posts. Also added a function to erase away error massage from text fields

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -5,11 +5,13 @@ import {
   GET_POSTS,
   GET_POST,
   POST_LOADING,
-  DELETE_POST
+  DELETE_POST,
+  CLEAR_ERRORS
 } from './types';
 
 // Add Post
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
   axios
     .post('/api/posts', postData)
     .then(res =>
@@ -111,8 +113,27 @@ export const removeLike = id => dispatch => {
 
 // Add Comment
 export const addComment = (postId, commentData) => dispatch => {
+  dispatch(clearErrors());
   axios
     .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Delete Comment
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios
+    .delete(`/api/posts/comment/${postId}/${commentId}`)
     .then(res =>
       dispatch({
         type: GET_POST,
@@ -131,5 +152,12 @@ export const addComment = (postId, commentData) => dispatch => {
 export const setPostLoading = () => {
   return {
     type: POST_LOADING
+  };
+};
+
+// Clear error message
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS
   };
 };
