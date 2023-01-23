2ad9bd378b0488d08156593cf2ff1cc166d11a63
Fahim Morshed <famambission491@gmail.com>
2023-01-24 04:30:07 +1100

    Add key & secret to key file

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys_prod');
-} else {
-  module.exports = require('./keys_dev');
-}
+module.exports = {
+  mongoURI: 'mongodb+srv://fahim86:hopeful1986@cluster0.p4b3cp6.mongodb.net/?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
+};
